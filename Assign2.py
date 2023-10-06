82e6d91ab52731f9b3d9143313c39562ef71b812
Ifera3 <144359377+Ifera3@users.noreply.github.com>
2023-10-06 12:45:25 -0700

    done

--- Assign2.py
+++ Assign2.py
@@ -4,3 +4,15 @@ Asks the user to enter in 2 numbers that can be float values
 Ask the user if one of the numbers is the hypotenuse of a right triangle
 Calculates the length of the missing side and rounds it to 1 decimal place.
 """
+import math
+
+x = float(input("Enter a number: "))
+y = float(input("Enter anouther number: "))
+
+_isHypoth = bool(input("Is one number the hypothenuse? (enter Ture or False): "))
+
+if _isHypoth:
+    hyp = math.sqrt((max(x, y) ** 2) - (min(x, y) ** 2))
+else:
+    hyp = math.sqrt((x ** 2) + (y ** 2))
+print(f"The hypothenuse is {round(hyp, 1)}")
